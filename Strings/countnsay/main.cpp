8249cdb6d3fe0ec4d84cb8ade80ac6a3accd1716
SurajAnil <includeme.h@gmail.com>
2017-06-07 16:41:28 -0700

    added MIT license agreement

--- Strings/countnsay/main.cpp
+++ Strings/countnsay/main.cpp
@@ -1,13 +1,36 @@
 /*
- *	Author: Suraj Bennur
  * The count-and-say sequence is the sequence of integers beginning as follows:	
+ * 
  * 1, 11, 21, 1211, 111221,...
  * 1 is read off as one 1 or 11.
  * 11 is read off as two 1s or 21.
  * 21 is read off as one 2, then one 1 or 1211.
  * Given an integer n, generate the nth sequence.
  * Note: The sequence of integers will be represented as a string.
- * compiler version: 
+ * 
+ * compiler version: gcc version 5.4.0 20160609
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Benn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 #include <iostream>
 
